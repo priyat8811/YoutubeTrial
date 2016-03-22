91c58ecea7530dcdd224a1f7fab0ec196747cc7f
sourcebitspriyanka <priyankat@sourcebitss-iMac-6.local>
2016-03-22 11:39:38 +0530

    First commit

---
+++ app/src/main/java/com/example/priyankat/youtubetrial/Config.java
@@ -0,0 +1,13 @@
+package com.example.priyankat.youtubetrial;
+
+/**
+ * Created by priyankat on 14/03/16.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAI31sJdoynf4Hr0_w8616iVO2-hLD2Oso";
+
+}
